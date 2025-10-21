7b7c9b42a4f90780f0ec074e974e21effad0c15d
PhelipeRamoss <phrcostaa@gmail.com>
2025-10-21 09:39:20 -0300

    Delete src directory

--- src/routes/relatorioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const relatorioController = require('../controllers/relatorioController');
-
-router.get('/alunos-por-turma/:turmaId', relatorioController.alunosPorTurma);
-router.get('/professores-por-disciplina/:disciplinaId', relatorioController.professoresPorDisciplina);
-router.get('/boletim/:alunoId/:periodo', relatorioController.boletimPorPeriodo);
-
-module.exports = router;
